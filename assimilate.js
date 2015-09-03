0e18a8e324f15127bb7a46c3262d187fe5e40119
Gerald Wodni <gerald.wodni@gmail.com>
2015-09-03 02:02:31 +0200

    stack effect isolated, very basic rendering working

--- assimilate.js
+++ assimilate.js
@@ -8,6 +8,7 @@ var jsdom = require("jsdom");
 var jquery = require("jquery");
 var async = require("async");
 var util = require("util");
+var _ = require("underscore");
 
 function readFile( filename, callback ) {
 
@@ -49,6 +50,7 @@ function readFile( filename, callback ) {
                     html: $secName.next().html()
                 };
             });
+            word.stackEffect = getStackEffect( sections );
             word.sections = sections;
 
             console.log( "WORD".bold.green, word.list.red, word.basename.yellow, word.name );
@@ -118,36 +120,111 @@ function readWordsets( prefix, callback ) {
     });
 }
 
-readWordsets( "latex-html", function( err, wordSetArray ) {
-    if( err )
-        throw err;
+function getStackEffect( sections ) {
+    var stackEffects = {
+        html: {},
+        plain: {}
+    }
 
-    var wordSets = {};
-    wordSetArray.forEach( function( wordSet ) {
-        if( wordSet !== null )
-            wordSets[ wordSet.name ] = wordSet;
+    /* section names containing stack effects */
+    var sectionPattern = /initialization|interpretation|compilation|run-time|execution|\s+/i;
+    _.each( sections, function( section ) {
+        if( !sectionPattern.test( section.name ) )
+            return;
+
+        /* get first paragraph */
+        var stackEffect = section.html.split(/<p\s*\/?>/)[0].trim();
+
+        /* check if real stack effect or comment */
+        if( /.*\(.*\).*/g.test( stackEffect ) ) {
+
+            /* strip html */
+            var plainStackEffect = stackEffect.replace( /<\/?[a-z]+>/gi, "" );
+            plainStackEffect = plainStackEffect.replace( /&gt;/g, ">" );
+            plainStackEffect = plainStackEffect.replace( /&lt;/g, "<" );
+            plainStackEffect = plainStackEffect.replace( /&quot;/g, '"' );
+            plainStackEffect = plainStackEffect.replace( /\n\s+/g, "\n" );
+
+            stackEffects.html[ section.name ] = stackEffect;
+            stackEffects.plain[ section.name ] = plainStackEffect;
+        }
     });
+
+    return stackEffects;
+}
+
+
+function assimilateAll( filename ) {
+    if( typeof filename  == "unknown" )
+        throw new Error( "specify filename" );
+
+    readWordsets( "latex-html", function( err, wordSetArray ) {
+        if( err )
+            throw err;
+
+        var wordSets = {};
+        wordSetArray.forEach( function( wordSet ) {
+            if( wordSet !== null )
+                wordSets[ wordSet.name ] = wordSet;
+        });
+
+        fs.writeFile( filename, JSON.stringify( wordSets, null, 4 ) );
+        
+        //console.log( "WordSets".bold.magenta, util.inspect( wordSets, { depth: null, colors: true } ) );
+    });
+}
+
+function assimilateSelection() {
+    readDir( "latex-html/selected", function( err, wordArray ) {
+        console.log( "DONE".bold.yellow );
+        if( err )
+            throw err;
     
-    console.log( "WordSets".bold.magenta, util.inspect( wordSets, { depth: null, colors: true } ) );
-});
+        var words = {}
+        wordArray.forEach( function( word ) {
+            words[ word.basename ] = word;
+        });
+    
+        console.log( "WORDS".bold.magenta, words );
+    });
+}
 
-//readDir( "latex-html/selected", function( err, wordArray ) {
-//    console.log( "DONE".bold.yellow );
-//    if( err )
-//        throw err;
-//
-//    var words = {}
-//    wordArray.forEach( function( word ) {
-//        words[ word.basename ] = word;
-//    });
-//
-//    console.log( "WORDS".bold.magenta, words );
-//});
+function assimilateSingleWord() {
+    readFile( "latex-html/core/PARSE.html", function( err, word ) {
+        if( err )
+            throw err;
 
-//readFile( "latex-html/core/PARSE.html", function( err, word ) {
-//    if( err )
-//        throw err;
-//
-//    console.log( "Word".bold.green, word );
-//});
+        console.log( "Word".bold.green, word );
+    });
+};
 
+
+function parseHtml( filename ) {
+    var wordSets = JSON.parse( fs.readFileSync( filename ) );
+
+    var i = 0;
+    var sectionNames = [];
+
+    _.each( wordSets, function( wordSet ) {
+        //if( wordSet.name != "core" ) return;
+
+        _.each( wordSet.words, function( word, name ) {
+
+            var stackEffect = getStackEffect( word.sections );
+
+            console.log( wordSet.name, word.name, stackEffect );
+
+            _.each( word.sections, function( section, sectionName ) {
+                sectionNames.push( sectionName );
+            });
+        });
+    });
+
+    console.log( "Sections:", _.uniq( sectionNames ) );
+}
+
+//parseHtml( process.argv[2] );
+
+//assimilateSingleWord();
+//assimilateSelection();
+assimilateAll( process.argv[2] );
