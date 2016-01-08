23dfa5759044323f4e084d510534c26a8d74e8b9
Gerald Wodni <gerald.wodni@gmail.com>
2016-01-08 18:36:59 +0100

    user settings added, user management started

--- userManagement.js
+++ userManagement.js
@@ -2,11 +2,13 @@
 // (c)copyright 2015 by Gerald Wodni <gerald.wodni@gmail.com>
 
 var md5 = require("md5");
+var _   = require("underscore");
 
 module.exports = {
     setup: function( k ) {
 
         var kData = k.getData();
+        var kDb = k.getDb();
         var vals = k.setupOpts.vals;
 
         k.router.get("/confirm/:hash", function( req, res, next ) {
@@ -71,9 +73,64 @@ module.exports = {
             });
         });
         k.router.get("/profile/change-password", function( req, res ) {
-            k.jade.render( req, res, "changePassword", vals( req, { title: "Change Password" } ) );
+            k.jade.render( req, res, "changePassword", vals( req ) );
         });
 
+        /* settings */
+        function renderProfileSettings( req, res, next, obj ) {
+            kData.users.readWhere("name", [req.session.loggedInUsername], function( err, user ) {
+                if( err ) return next( err );
+                k.jade.render( req, res, "settings", vals( req, _.extend( obj || {}, { dailyDigest: user[0].dailyDigest } ) ) );
+            });
+        }
+        k.router.post("/profile/settings", function( req, res, next ) {
+            k.postman( req, res, function() {
+                kDb.query("UPDATE users SET dailyDigest=? WHERE name=?", [ req.postman.exists( "dailyDigest" )?1:0, req.session.loggedInUsername ], function( err ) {
+                    var message;
+                    if( err )
+                        message = { error: err.toString() };
+                    else
+                        message = { success: "settings saved" };
+                    renderProfileSettings( req, res, next, message );
+                });
+            });
+        });
+        k.router.get("/profile/settings", function( req, res, next ) { renderProfileSettings( req, res, next ); } );
+
+        /* users */
+        function renderManageUsers( req, res, next, obj ) {
+
+            kData.users.readAll( function( err, users ) {
+                if( err )
+                    return next( err );
+
+                users.forEach( function( user ) {
+                    user.emailMd5 = md5( user.email );
+                });
+
+                k.jade.render( req, res, "manageUsers", vals( req, _.extend( obj || {}, {
+                    users: users
+                })));
+            });
+        }
+        k.router.post("/profile/manage-users", function( req, res, next ) {
+            k.postman( req, res, function() {
+                kData.users.update( req.postman.id("user"), { state: req.postman.alpha("state") }, function( err ) {
+                    var message;
+                    if( err )
+                        message = { type: "danger", text: err };
+                    else
+                        message = { type: "success", title: "success", text: "User state changed" };
+
+                    renderManageUsers( req, res, next, { messages: [message] } );
+                });
+            });
+        });
+        k.router.get("/profile/manage-users", function( req, res, next ) {
+            renderManageUsers( req, res, next );
+        });
+
+        /* profile */
         k.router.get("/profile", function( req, res, next ) {
             k.setupOpts.renderUser( req.session.loggedInUsername, req, res, next );
         });
