fd8634350d9d80aa0b16e66bd58fca825b7d8974
Gerald Wodni <gerald.wodni@gmail.com>
2022-09-16 01:29:29 +0200

    sql: contributionState fixed, contributionStateOrder updated

--- sql/contributionState.sql
+++ sql/contributionState.sql
@@ -4,6 +4,7 @@ COALESCE(
     (SELECT newState
     FROM replies
     WHERE contribution=contributionId
+    AND newState IS NOT NULL
     ORDER BY replies.created DESC
     LIMIT 1)
 ,   (SELECT 'informal'
@@ -15,7 +16,11 @@ COALESCE(
 -- provide sorting for state
 CREATE OR REPLACE FUNCTION contributionStateOrder (state VARCHAR(255)) RETURNS INT RETURN
 CASE
-    WHEN state='informal' THEN 0
-    WHEN state='rejected' THEN 3
-    ELSE 10
+	WHEN state='voting' THEN 0
+    WHEN state='formal' THEN 10
+    WHEN state='informal' THEN 20
+    WHEN state='accepted' THEN 40
+    WHEN state='rejected' THEN 50
+    WHEN state='retiered' THEN 60
+    ELSE 100
 END;
